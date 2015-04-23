d66a4544f703233e1834b7ddf6c5d45a86471976
Eric Windisch <eric@windisch.us>
2015-04-23 14:12:21 -0400

    gofmt
    Signed-off-by: Eric Windisch <eric@windisch.us>

--- udocker.go
+++ udocker.go
@@ -3,15 +3,15 @@ package main
 import (
 	"os"
 	"os/exec"
-    "strconv"
+	"strconv"
 )
 
 func main() {
 	uid := strconv.Itoa(os.Getuid())
 
-    cmdargs := append([]string{"run", "-u", uid, "--rm", "-i"}, os.Args[1:]...)
-    cmd := exec.Command("docker", cmdargs...)
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
-    cmd.Run()
+	cmdargs := append([]string{"run", "-u", uid, "--rm", "-i"}, os.Args[1:]...)
+	cmd := exec.Command("docker", cmdargs...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Run()
 }
