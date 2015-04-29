5bda9feda079d34f0769b2cf124002b7d3f857cc
Eric Windisch <eric@windisch.us>
2015-04-29 14:35:52 -0400

    Use absolute path for Docker cli
    Signed-off-by: Eric Windisch <eric@windisch.us>

--- udocker.go
+++ udocker.go
@@ -10,7 +10,7 @@ func main() {
 	uid := strconv.Itoa(os.Getuid())
 
 	cmdargs := append([]string{"run", "--cap-drop", "ALL", "-u", uid, "--rm", "-i", "--"}, os.Args[1:]...)
-	cmd := exec.Command("docker", cmdargs...)
+	cmd := exec.Command("/usr/bin/docker", cmdargs...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
