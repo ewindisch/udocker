144dfc4fae3d5ec43b82970fb1d9393dfa1d88f1
LordFPL <lordfpl@hotmail.com>
2015-05-06 09:34:44 +0200

    Add gid
    Hello, i can't understand how gid can be added without get it ;)
    I'm not a go developer... so my code is certainly ugly... but it's work :)

--- udocker.go
+++ udocker.go
@@ -8,8 +8,10 @@ import (
 
 func main() {
 	uid := strconv.Itoa(os.Getuid())
+	gid := strconv.Itoa(os.Getgid())
+	uidgid := uid+":"+gid
 
-	cmdargs := append([]string{"run", "--cap-drop", "ALL", "-u", uid, "--rm", "-i", "--"}, os.Args[1:]...)
+	cmdargs := append([]string{"run", "--cap-drop", "ALL", "-u", uidgid, "--rm", "-i", "--"}, os.Args[1:]...)
 	cmd := exec.Command("/usr/bin/docker", cmdargs...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
